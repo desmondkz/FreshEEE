46e1de6f99cc60d8896337997dea5d974c0759f6
kz <kanin.sae-tiew@outlook.com>
2020-09-10 16:48:14 +0800

    updated user class

--- app/src/main/java/com/ntu/fresheee/User.java
+++ app/src/main/java/com/ntu/fresheee/User.java
@@ -4,10 +4,13 @@ public class User {
 
     public String email, fullname;
 
-    public User() {
+    public User(){
+
+    }
+
+    public User(String fullname, String email) {
 //      this is class to store user information
         this.fullname = fullname;
         this.email = email;
     }
-
 }
