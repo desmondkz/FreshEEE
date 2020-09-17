ffb020b3c7ddf943054fa475097a2b3b106ca815
desmondkz <39067134+desmondkz@users.noreply.github.com>
2020-09-18 01:09:02 +0800

    added buttom navigation in travel declaration WebView

--- app/src/main/java/com/ntu/fresheee/WebViewHeathDeclarationActivity.java
+++ app/src/main/java/com/ntu/fresheee/WebViewHeathDeclarationActivity.java
@@ -1,10 +1,16 @@
 package com.ntu.fresheee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.MenuItem;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class WebViewHeathDeclarationActivity extends AppCompatActivity {
 
@@ -15,6 +21,34 @@ public class WebViewHeathDeclarationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view_heath_declaration);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+//        //Set Discovery Selected
+//        bottomNavigationView.setSelectedItemId(R.id.discovery);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.discovery:
+                        startActivity(new Intent(getApplicationContext(), DiscoveryActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
 
         healthDeclarationwebView = (WebView) findViewById(R.id.health_declaration_webview);
         healthDeclarationwebView.setWebViewClient(new WebViewClient());
