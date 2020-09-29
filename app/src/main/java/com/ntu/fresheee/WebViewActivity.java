0a8f06831f084b285760c448535ea601fd5c90f7
desmondkz <39067134+desmondkz@users.noreply.github.com>
2020-09-29 10:42:42 +0800

    updated UI layout in webview activity (removed buttom navigation and implemented back button at the top)

--- app/src/main/java/com/ntu/fresheee/WebViewActivity.java
+++ app/src/main/java/com/ntu/fresheee/WebViewActivity.java
@@ -22,30 +22,8 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        //Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        //Perform ItemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.discovery:
-                        startActivity(new Intent(getApplicationContext(), DiscoveryActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.settings:
-                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
-            }
-        });
+        getSupportActionBar().getTitle();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         url = getIntent().getStringExtra("url");
         webView = (WebView) findViewById(R.id.main_webview);
